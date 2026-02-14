4e392054c4af4c97e6906b972f1db447cea917fa
Damian  Hutsberg <irod@Artems-MacBook-Air.local>
2026-02-14 10:09:08 +0200

    ts suka3

--- app/[lang]/project-roadmap/page.tsx
+++ app/[lang]/project-roadmap/page.tsx
@@ -23,5 +23,5 @@ export async function generateMetadata({ params }: { params: Promise<{ lang: str
 export default async function Page({ params }: { params: Promise<{ lang: string }> }) {
   const { lang } = await params
   const t = getTranslations(lang)
-  return <RoadmapTokenomics translations={t} />
+  return <RoadmapTokenomics translations={t}  />
 }
